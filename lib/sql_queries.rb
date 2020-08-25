79a7223dbca324cf02d1a6b952d9ff7001aa6740
Daniel Gould <dmgould310@gmail.com>
2020-08-25 13:34:15 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
    FROM projects 
    INNER JOIN pledges  
    ON projects.id = pledges.project_id 
-   GROUP BY projects.title, DESC LIMIT;"
+   ORDER BY projects.title DESC 
+   LIMIT 2;"
 end
 
 
